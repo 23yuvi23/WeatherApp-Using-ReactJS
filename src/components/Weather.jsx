f0c7f4867293f39fd4ade62129e32b788aca8fd6
23yuvi23 <yogeshpaliwal9493@gmail.com>
2026-01-21 12:02:13 +0530

    Search Functionality done Fully working APP

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import "./Weather.css"
 import search_icon from "../assets/search.png"
 import clear_icon from "../assets/clear.png"
@@ -11,34 +11,51 @@ import wind_icon from "../assets/wind.png"
 
 
 const Weather = () => {
+    //search functionality
+    const inputRef = useRef()
+
     //state variable to store data coming from API
     const [weatherData, setWeatherData] = useState(false)
 
-    const allIcons = {
-        "01d" : clear_icon,
-        "01n" :clear_icon,
-        "02d" : cloud_icon,
-        "02n":cloud_icon,
-        "03d" : drizzle_icon,
-        "03n":drizzle_icon,
-        "09d" : rain_icon,
-        "09n":rain_icon,
-        "10d" : snow_icon,
-        "10n":snow_icon
-    }
-
+const allIcons = {
+    "01d": clear_icon,
+    "01n": clear_icon,
+    "02d": cloud_icon,
+    "02n": cloud_icon,
+    "03d": cloud_icon,
+    "03n": cloud_icon,
+    "04d": drizzle_icon, // Broken clouds ko drizzle ya cloud pe map karein
+    "04n": drizzle_icon,
+    "09d": rain_icon,
+    "09n": rain_icon,
+    "10d": rain_icon,    // Ye missing tha (Rain)
+    "10n": rain_icon,
+    "11d": rain_icon,    // Thunderstorm
+    "11n": rain_icon,
+    "13d": snow_icon,
+    "13n": snow_icon,
+}
     // api adding 
     const search = async (city) => {
+        if (city === "") {
+        alert("Enter City Name");
+        return;
+        }
         try{
             const url = 
-            `https://api.openweathermap.org/data/2.5/weather?q=
-            ${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`
             const response = await fetch(url);
             const data = await response.json();
-            
+
+            if (!response.ok) {
+            alert(data.message);
+            return;
+        }
+
             // console.log(url);
             // console.log(data);
-            const icon =  allIcons[data.weather[0].icon] || clear_icon
+            const iconCode = data.weather[0].icon;
+            const icon = allIcons[iconCode] || clear_icon;
             setWeatherData({
                 humidity: data.main.humidity,
                 windSpeed :data.wind.speed,
@@ -47,21 +64,22 @@ const Weather = () => {
                 icon: icon
             })
         } catch (error) {
-
+            setWeatherData(false);
+            console.error("Error in fetching data");
         }
     }
 
     //call search () whenever component gets loaded 
     useEffect(()=>{
-        search("New York");
+        search("North Pole");
     },[])
 
 
   return (
     <div className='weather'>
         <div className="search-bar">
-            <input type="text" name="" id="" placeholder='search'/>
-            <img src={search_icon} alt="search"/>
+            <input ref={inputRef} type="text" name="" id="" placeholder='search'/>
+            <img onClick={()=>search(inputRef.current.value)} src={search_icon} alt="search"/>
         </div>
         <img src={weatherData.icon} alt="clear" className='weather-icon'/>
         <p className='temperature'>{weatherData.temperature}°C</p>
